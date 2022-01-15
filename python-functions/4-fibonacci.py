e5b7f6ee36e2b40a09541f90fc6334bda027341e
OlasupoTosin <olasupofrancis90@gmail.com>
2022-01-15 16:25:59 +0000

    task1-3

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,22 @@
+# user_input = input("enter a fibonacci_sequence: ")
+# def fibonacci_sequence(n):
+#     print(fibonacci_sequence(6))
+#     print(fibonacci_sequence(1))
+#     print(fibonacci_sequence(0))
+#     print(fibonacci_sequence(20))
+#     return (n)
+# fibonacci_sequence(user_input)
+
+# print("""\
+# Usage: thingy [OPTIONS]
+#      -h                        Display this usage message
+#      -H hostname               Hostname to connect to
+# """)
+
+print(
+"""\
+    Usage: thingy [OPTIONS]
+     -h                        Display this usage message
+     -H hostname               Hostname to connect to """
+)
+
---
+++ python-functions/README.md
@@ -0,0 +1,12 @@
+This directory is for the Pyhton Function Tasks.
+it answers the following questions under the Funtion topic in python:
+
+What are functions?
+How to define and call a function?
+What are parameters and arguments?
+What are the role of the return statement in functions?
+What are the difference between built in functions and user-defined functions?
+How to write functions to solve specific tasks and improve code reusability?
+
+AUTHOR : TosinOlasupo
+School: ALX SWE
