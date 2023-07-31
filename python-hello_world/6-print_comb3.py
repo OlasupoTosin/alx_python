26faf6ef7e844d0b8d81d4ae48fd5d63dc0b227d
OlasupoTosin <olasupofrancis90@gmail.com>
2023-07-31 15:36:00 +0100

    task-6_reviewed

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,6 +1,6 @@
 for number in range(10):
     for numbers in range(10):
         if(number == 8 and numbers == 9):
-            print("{}" .format(str(number) + str(numbers)), end=", ")
+            print("{}" .format(str(number) + str(numbers)))
         elif(numbers > number):
-            print("{}" .format(str(number) + str(numbers)), end="\n")
+            print("{}" .format(str(number) + str(numbers)) + ", ", end="")
