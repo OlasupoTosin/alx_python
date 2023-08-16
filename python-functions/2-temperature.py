cb035f5c2e9f890fe56034784d464b6840ab297c
OlasupoTosin <olasupofrancis90@gmail.com>
2023-08-16 09:57:14 +0100

    review_2

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -3,8 +3,8 @@
 def convert_to_celsius(fahrenheit):
     celsius = (fahrenheit - 32) / 1.8
     return(celsius)
-# convert_to_celsius = __import__('2-temperature').convert_to_celsius
+""" # convert_to_celsius = __import__('2-temperature').convert_to_celsius
 print(convert_to_celsius(100))
 print(convert_to_celsius(-40))
 print(convert_to_celsius(-459.67))
-print(convert_to_celsius(32))
+print(convert_to_celsius(32)) """
