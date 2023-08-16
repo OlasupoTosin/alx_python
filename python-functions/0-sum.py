546030adef1f5605c5279ab13f70bcd435415bac
OlasupoTosin <olasupofrancis90@gmail.com>
2023-08-16 09:53:15 +0100

    review_1

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -3,7 +3,7 @@ def add(a, b):
     result = a + b
     return result
 
-# add = __import__('0-sum').add
+""" # add = __import__('0-sum').add
 print(add(1, 2))
 print(add(98, 0))
-print(add(100, -2))
+print(add(100, -2)) """
