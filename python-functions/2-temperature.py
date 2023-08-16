607ca652a093daf7516b3400b9010d88e7afcb91
OlasupoTosin <olasupofrancis90@gmail.com>
2023-08-16 10:38:02 +0100

    rounded_up_th_return_value

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -2,7 +2,7 @@
 
 def convert_to_celsius(fahrenheit):
     celsius = (fahrenheit - 32) / 1.8
-    return(celsius)
+    return(round(celsius))
 """ # convert_to_celsius = __import__('2-temperature').convert_to_celsius
 print(convert_to_celsius(100))
 print(convert_to_celsius(-40))
